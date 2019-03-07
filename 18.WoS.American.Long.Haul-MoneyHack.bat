2ae5cbd67195ae8a53034d44e122dc17f293da02
Steffan (Aeneas of Troy) <33581978+aeneasoftroy@users.noreply.github.com>
2019-03-07 03:29:46 +0100

    Add files via upload

---
+++ 18.WoS.American.Long.Haul-MoneyHack.bat
@@ -0,0 +1,32 @@
+SETLOCAL ENABLEDELAYEDEXPANSION
+echo off
+color 0a
+cls
+echo. && echo 18 Wheels of Steel: American Long Haul money hack && echo. && echo %date% %time% Searching for game save files ...
+if not exist "%userprofile%\documents\18 WoS American Long Haul" color 0c && echo ERROR: Game save directory not found && pause && exit
+if not exist "%userprofile%\documents\18 WoS American Long Haul\save\1" color 0c && echo ERROR: Game save directory not found && echo Start the game and save it once, then run this again. && pause && exit
+echo %date% %time% OK: Game save files found in:
+echo "%userprofile%\documents\18 WoS American Long Haul\save\1"
+
+timeout 1 > NUL
+
+echo %date% %time% Changing money_account value ...
+
+cd "%userprofile%\documents\18 WoS American Long Haul\save\1"
+
+echo before hack:
+findstr /i "money_account" game.sii
+
+for /f "delims=" %%a in (game.sii) do (
+    SET s=%%a
+    SET s=!s:money_account^: =money_account^: 999!
+    echo !s!
+) >> tmp.txt
+
+move game.sii game.sii.bak > NUL
+move tmp.txt game.sii > NUL
+
+echo after hack:
+findstr /i "money_account" game.sii
+
+pause
